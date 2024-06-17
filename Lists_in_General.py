1154d5325894d0ae27a4fe7c861b8a9262cc12e4
DianaGit-Hub <dbvalencia2025@sjrcla.org>
2024-06-17 13:31:05 -0700

    Create Lists_in_General.py

---
+++ Lists_in_General.py
@@ -0,0 +1,8 @@
+x = [10,30,50]
+print ("This is a list of numbers i put on the spot", x)
+
+x = ['salsa', 'church', 'tabke']
+print("These are other things", x)
+
+x = ['chaos', 'bread', 94, False, 2]
+print("I dont know what to put anymore", x)
