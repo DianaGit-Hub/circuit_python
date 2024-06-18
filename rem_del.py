34144537bc0845fa8eb3ab432c2bd18f96cf269f
DianaGit-Hub <dbvalencia2025@sjrcla.org>
2024-06-18 12:53:19 -0700

    Create rem_del.py

---
+++ rem_del.py
@@ -0,0 +1,21 @@
+# Hello World
+my_list = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d",]
+print(my_list)
+# Using the sort() method to sort the list
+my_list.sort()
+print(my_list)
+
+# pop Clown
+my_list = ["STEAM", "Clown", "Productions"]
+print(my_list)
+x = my_list.pop(1)
+print(my_list)
+print(x)
+
+# del Production
+my_list = ["STEAM", "Clown", "Productions"]
+print(my_list)
+del my_list[2]
+print(my_list)
+my_list.remove("STEAM")
+print(my_list)
