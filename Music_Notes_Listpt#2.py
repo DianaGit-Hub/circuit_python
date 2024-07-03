eba4e349a1b4fbdd50e7df27e00a05d895ab3598
DianaGit-Hub <dbvalencia2025@sjrcla.org>
2024-07-03 13:25:41 -0700

    Create Music_Notes_Listpt#2.py

---
+++ Music_Notes_Listpt#2.py
@@ -0,0 +1,16 @@
+from adafruit_circuitplayground import cp
+
+from adafruit_circuitplayground import cp
+
+#twinkle twinkle little star below
+note = [262,262,392,392,440,440,392]
+
+# c,d,e,f,g,a,b,c
+
+#note = [262,294,330,349,392,440,494,523]
+duration = [2,1,1,1,1,1,2,1]
+
+for i in range(8):
+    cp.pixels[i] = (25,0,60)
+    cp.play_tone(note[i], duration[i])
+    cp.pixels[i] = (0,0,0)
