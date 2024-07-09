b0e8cc250abc3f803f4420d51f4d8206ea18397e
DianaGit-Hub <dbvalencia2025@sjrcla.org>
2024-07-09 13:18:23 -0700

    Create Raffle_Winner.py

---
+++ Raffle_Winner.py
@@ -0,0 +1,6 @@
+import random
+participants = ( 'Dandy George', 'Kurtis Perez', 'Janice', 'MkCloven the 3rd', 'Fernanda Flores', 'Patricia Jones', 'Gerome adams')
+
+winner = random.choice(participants)
+
+print('And the lucky person is...', (winner))
