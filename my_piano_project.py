4581121d01043d3d4a28c24454fde97c550d1d0d
DianaGit-Hub <dbvalencia2025@sjrcla.org>
2024-07-16 13:38:17 -0700

    Update my_piano_project.py

--- my_piano_project.py
+++ my_piano_project.py
@@ -14,53 +14,48 @@ cp.touch_a5 = DigitalInOut(board.A5)
 cp.touch_a6 = DigitalInOut(board.A6)
 cp.touch_a7 = DigitalInOut(board.A7)
 
-note = [262,294,330,349,392,440,494]
-duration = [1,1,1,1,1,1,1]
+#note = [262,294,330,349,392,440,494]
+#duration = [1,1,1,1,1,1,1]
 direction = Direction.INPUT
 
 
 
 while True:
-    if cp.button_a:
-        while (cp.button_a):  
-		pass
-        print("pressed a")
-        cp.play_tone(note[i],duration[i])
-        
     
-    elif cp.touch_a1:
+    if cp.touch_a1:
         cp.pixels.fill((15,0,0))
- #       cp.play_tone(note[1])
+ #       cp.play_tone(262)
         print ("A1 has been touched")
+        
         
     elif cp.touch_a2:
         cp.pixels.fill((0,15,0))
- #       cp.play_tone(note[2])
+ #       cp.play_tone(294)
         print ("A2 has been touched")
         
     elif cp.touch_a3:
         cp.pixels.fill((0,0,15))
- #       cp.play_tone(note[3])
+ #       cp.play_tone(330)
         print ("A3 has been touched")
         
     elif cp.touch_a4:
         cp.pixels.fill((15,15,0))
- #       cp.play_tone(note[4])
+ #       cp.play_tone(349)
         print ("A4 has been touched")
         
     elif cp.touch_a5:
         cp.pixels.fill((0,15,15))
- #       cp.play_tone(note[5])
+ #       cp.play_tone(392)
         print ("A5 has been touched")
         
     elif cp.touch_a6:
         cp.pixels.fill((15,0,15))
- #       cp.play_tone(note[6])
+ #       cp.play_tone(440)
         print ("A6 has been touched")
         
     elif cp.touch_a7:
         cp.pixels.fill((15,15,15))
- #       cp.play_tone(note[7])
+ #       cp.play_tone(494)
         print ("A7 has been touched")
         
     else:
@@ -68,3 +63,72 @@ while True:
         cp.stop_tone()
     
     time.sleep(1)
+
+
+
+
+__________________________________________________________________________________________________________________________
+
+
+
+
+
+
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import time
+from adafruit_circuitplayground import cp
+
+
+#define ir as a digital device connected
+ir = DigitalInOut(board.A1) 
+ir2 = DigitalInOut(board.A2)
+ir3 = DigitalInOut(board.A3) 
+ir4 = DigitalInOut(board.A4)
+ir5 = DigitalInOut(board.A5) 
+ir6 = DigitalInOut(board.A6)
+ir7 = DigitalInOut(board.A7)
+
+#key notes = [262,294,330,349,392,440,494]
+
+#configure ir as INPUT
+ir.direction = Direction.INPUT
+
+while True:
+    #read ir value (True or False)
+ #   print(ir.value,ir2.value,ir3.value,ir4.value,ir5.value,ir6.value,ir7.value)
+ #   time.sleep(1)
+    
+#all the keys down below
+    if ir == True:
+        cp.pixels.fill((15,0,0))
+#        cp.play_tone(262)
+        
+    elif ir2 == True:
+        cp.pixels.fill((0,15,0))
+#        cp.play_tone(294)
+    
+    elif ir3 == True:
+        cp.pixels.fill((0,0,15))
+#        cp.play_tone(330)
+        
+    elif ir4 == True:
+        cp.pixels.fill((15,15,0))
+#        cp.play_tone(349)
+        
+    elif ir5 == True:
+        cp.pixels.fill((0,15,15))
+#        cp.play_tone(392)
+        
+    elif ir6 == True:
+        cp.pixels.fill((15,0,15))
+#        cp.play_tone(440)
+        
+    elif ir7 == True:
+        cp.pixels.fill((15,15,15))
+#        cp.play_tone(494)
+
+    else:
+        cp.pixels.fill((0,0,0))
+        cp.stop_tone()
+
